bfed132b0f1280faaedbe0b00f6727d07351b7ea
Daniel Kueffer <daniel_kueffer@hotmail.com>
2015-04-17 11:23:28 +0200

    initial configuration

---
+++ src/main/java/com/danielkueffer/filehosting/AuthController.java
@@ -0,0 +1,15 @@
+package com.danielkueffer.filehosting;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class AuthController implements Serializable {
+
+	
+	private static final long serialVersionUID = 2059533199704287584L;
+
+}
